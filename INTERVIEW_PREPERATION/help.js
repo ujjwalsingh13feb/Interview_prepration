9b1103b57ca30f7c26138427918425632a4a4c4a
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-03-15 16:06:25 +0530

    COFFE SHOP

---
+++ INTERVIEW_PREPERATION/help.js
@@ -0,0 +1,5 @@
+// let chars = [3,1,2,3,2,1];
+// let uniqueChars = [...new Set(chars)];
+
+// console.log(uniqueChars.join(" "));
+
---
+++ INTERVIEW_PREPERATION/string.js
@@ -0,0 +1,114 @@
+
+// String length
+// String slice()
+// String substring()
+// String substr()
+// String replace()
+// String replaceAll()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+
+
+
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String charAt()ru
+// String charCodeAt()
+// String split()
+
+
+
+// var str = "ujjwal singh"
+// let string= str.slice(0,9)
+
+// console.log(string)
+
+
+// var str = "ujjwal singh"
+// let string= str.substring(0,11)
+
+// console.log(string)
+
+
+// let text = "Please visit Microsoft!";
+// let newText = text.replace("Microsoft", "W3Schools");
+// console.log(newText)
+
+// To replace case insensitive, use a regular expression with an /i flag (insensitive):
+// let text = "Please visit Microsoft!";
+// let newText = text.replace(/MICROSOFT/i, "W3Schools");
+
+
+// let text = "Please visit Microsoft and Microsoft!";
+// let newText = text.replace(/Microsoft/g, "W3Schools");
+
+
+// let text = "I love cats. Cats are very easy to love. Cats are very popular."
+// text = text.replaceAll("Cats","Dogs");
+// text = text.replaceAll("cats","dogs")
+
+
+
+// console.log(text)
+
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1.concat(" ", text2);
+
+// let text4 = text1+" "+text2
+// console.log(text4)
+
+
+// let text1 = "     Hello World!     ";
+// let text2 = text1.trim();
+// console.log(text2)
+
+
+// Note
+// The padStart() method is a string method.
+
+// To pad a number, convert the number to a string first.
+
+// See the example below.
+
+// let text = "5";
+// let padded = text.padStart(4,"x");
+
+
+// Note
+// The padEnd() method is a string method.
+
+// To pad a number, convert the number to a string first.
+
+// See the example below.
+
+
+// let text = "5";
+// let padded = text.padEnd(4,"x");
+// console.log(padded)
+
+
+// let text = "ujjwal singh"
+// // let text2 =text.split(",")    // Split on commas
+// // text.split(" ")    // Split on spaces
+// // text.split("|")    // Split on pipe
+
+// console.log(text2)
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+// console.log(fruits.sort())
+
+const d = new Date()
+// console.log(d.getFullYear())
+// console.log(d.getMonth())
+// console.log(d.getDate())
+// console.log(d.setFullYear(2020))
+
+// let x = Math.max(10, 20);
+// console.log(x)
+
