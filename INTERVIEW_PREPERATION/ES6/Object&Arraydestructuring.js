79d9344885c26eb5b99f7019886a7df1df043b06
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-03-18 23:31:42 +0530

    add

---
+++ INTERVIEW_PREPERATION/ES6/Object&Arraydestructuring.js
@@ -0,0 +1,24 @@
+// Object and array desttcutturing
+
+// rest, spread, operater
+// puting the value singl way 
+var person ={};
+person.name = "ujjwal Singh"
+person.vill = "RamPur"
+person.post = "sasamusa"
+
+
+// puting the value multipul way
+
+var personDetail={
+    name: "ujjwal Singh",
+    vill : "Rampur",
+    post : "RamPur"
+
+}
+
+let {name,vill ,post }=personDetail
+console.log(name,vill,post)
+
+
+
