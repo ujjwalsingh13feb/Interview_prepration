a490528d220aba65df10cff8f41ce2267a389c01
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-03-18 21:54:49 +0530

    miscellaneous

--- miscellaneous/index.js
+++ miscellaneous/index.js
@@ -1,9 +1,157 @@
-function getName(){
-    var status = true
-    if(status){
-      const name = "ujjwal Singh"
-    }
-    console.log(name)
-  }
+// function getName(){
+//     var status = true
+//     if(status){
+//       const name = "ujjwal Singh"
+//     }
+//     console.log(name)
+//   }
  
-  getName()
+//   getName()
+
+
+// function alpha(a=100){
+//   ...
+// }
+
+// let ans = alpha()
+// console.log(ans)
+
+// function add(b){
+//   const a=100;
+//   a=b;
+//   return a
+// }
+
+// console.log(add(100))
+
+// let fname = "ujjwal";
+// let Lname="Singh"
+// const person = {fname,Lname}
+// console.log(person)
+
+// const email = "ujjwalsingh@gmail.com";
+// let name = "ujjwal";
+
+// email = "ujjwal@gmail.com"
+// name = "ujjwal singh"
+
+// console.log(email,name)
+
+// const arr = ["ujjwal","singh","prakash"]
+
+// arr[0]="chintu";
+// console.log(arr)
+
+///????
+
+// function getName(){
+//    name = "ujjwal singh";
+  
+// }
+
+// getName()
+// console.log(name)
+
+///??
+
+// How can you destructure the object passed inside the getData function?
+
+
+// function getData({ username, password }) {
+//   console.log(`Username: ${username}, Password: ${password}`);
+// }
+
+// getData({ username: "admin", password: "123445" });
+
+
+
+
+// var name 
+// console.log(name)
+
+////?
+
+// function getUrl(userId){
+//   return `https://example.com/${userId}`
+// }
+
+// let url = getUrl("ujjwal")
+// console.log(url)
+
+
+////\\\\
+// What is the output of the following?
+// var str ="ujjwal";
+// var {length}=str;
+
+// console.log(length);
+
+// What is the output of the following code?
+// let arr =  ["a","b","c","d","e"]
+
+// const func =([f,...r])=>r.reduce((a,c)=>a+c)+f
+
+// console.log(func(arr))
+
+////\\\\
+// What is the output of the following
+
+// console.log(name)
+// var name = "ujjwal Singh"
+
+///\\\\
+// What is the output of the following?
+
+// var arr = [1,2,3,4,5]
+
+// arr.map(a=>a*5)
+// console.log(arr)
+
+/////\\\\\
+
+// Are the functions same?
+// const sum =(a,b)=>a+b;
+// let add=sum(20,10)
+// console.log(add)
+
+// function sum(a,b){
+//   a+b
+// }
+// let add = sum(10,20)
+// console.log(add)
+
+/////\\\
+
+// What will be the output/
+
+// function getname(){
+//   var status = true;
+//   if (status){
+//     const name = "ujjwal"
+
+//   }
+//   console.log(name)
+// }
+// getname()
+
+///\\\
+
+// What is the output of the following
+
+// console.log(
+//   typeof[1,1,1,1]===typeof {val:1,val:1,val:1})
+
+/////\\\\
+// Which of the following is an implicit coercion?
+
+
+var prod= "10"*5; //   this is implicit coercion 
+console.log(prod)
+
+var join = "Hello " +5 +"th person"
+console.log(join)
+
+var b = String( 1024)
+console.log(b)
+
+
