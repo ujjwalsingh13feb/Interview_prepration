e166d0f155c4eb5d0b6508d4d87c6cbd389a1e50
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-03-22 17:47:58 +0530

    miscellaneous

--- miscellaneous/index.js
+++ miscellaneous/index.js
@@ -265,3 +265,19 @@
 // ReferenceError
 // SyntaxError
 // TypeError
+
+// reverse the number 
+var reverse = function (num) {
+    // Conver the number to a string, split it to an array, reverse it, and then re-join it
+    const reversedNumber = parseInt(
+        Math.abs(num).toString().split('').reverse().join('')
+    );
+
+    // Check for an invalid output
+    if (reversedNumber > 2147483647) {
+        return 0;
+    }
+
+    // Return the reversed number (negating it if the original number was negative)
+    return num < 0 ? -Math.abs(reversedNumber) : reversedNumber;
+};
