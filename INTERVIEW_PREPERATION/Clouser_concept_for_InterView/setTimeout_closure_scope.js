ce565a881e37aa04c89e47538261999e00732a19
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-03-22 20:30:24 +0530

    setTimout_clouser

---
+++ INTERVIEW_PREPERATION/Clouser_concept_for_InterView/setTimeout_closure_scope.js
@@ -0,0 +1,48 @@
+// setTimeout closure scope example ( Amazon interview question )
+
+// var arr = [1,2,3];
+// for(var  i=0;i<arr.length;i++){
+//     setTimeout(function(){
+//         console.log(i,arr[i]);
+//     }, i *1000)
+// }
+
+// console.log(`the value of i is ${i}`)
+
+
+// second way to ask the question 
+
+// let arr = [1,2,3];
+
+// function display(i){
+//     setTimeout(function(){
+//         console.log(i,arr[i])
+//     }, i*1000)
+// }
+
+// for(var  i=0;i<arr.length;i++){
+//     display(i)
+// }
+
+
+// third way to ask question always see the video 
+
+let arr = [1,2,3];
+
+function display(i){
+    var that = this
+    setTimeout(function(){
+        console.log(i,that[i])
+    }, i*1000)
+}
+
+for(var  i=0;i<arr.length;i++){
+    display.call(arr,i)
+}
+
+
+// scope;
+//closure;
+//async,for loops
+// call / apply,bind
+//arrow function
