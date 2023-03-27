0d5c829714cb3f8647995c494a73a5cb401e067e
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-03-27 23:42:51 +0530

    Dsa

---
+++ InterView_Dsa/primenumb.js
@@ -0,0 +1,19 @@
+var a = [5, 9, 63, 29, 35, 6, 55, 23]
+var prime = [];
+
+function isPrime(item) {
+    var identifier = item / 2;
+      for (var j = 2; j <= identifier; j++) {
+       if ((item % j) == 0) { // modulous
+        return false;
+       } 
+     }
+     return true;
+}
+for (var index = 0; index < a.length; index++) {
+  if (isPrime(a[index])) {
+      prime.push(a[index])
+  }
+}
+
+console.log(prime);
