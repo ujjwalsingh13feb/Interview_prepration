9cb365159dceb0b242e29b76f4e805bdc0c05769
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-03-29 16:03:49 +0530

    counter-app

---
+++ counter-app/src/component/Counter.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import  "../css/Counter.css"
+
+export const Counter = () => {
+   const [count,setCount] = useState(0)
+
+   const incCount =()=>{
+      setCount(count+1)
+   }
+
+   const decCount =()=>{
+     if(count>0){
+        setCount(count-1)
+     }else{
+        setCount(0)
+        alert("Zero limit rich" )
+        
+     }
+   }
+
+  return (
+    <>
+      <h1>Counter-App</h1>
+      <div className="counter-main">
+        <div className="counter">
+            <h1 className="show">{count}</h1>
+            <div className="btn">
+            <button onClick={incCount}>Increment</button>
+            <button onClick={decCount}>Decrement</button>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
