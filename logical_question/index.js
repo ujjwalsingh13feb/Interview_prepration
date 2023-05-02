76933db60daf67deeaafb8e15f29eba164118e24
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-05-02 14:26:07 +0530

    logical_question

---
+++ logical_question/index.js
@@ -0,0 +1,14 @@
+const name = "Hello Ujjwal Singh";
+const age =25;
+
+console.log(Number.isNaN(name));
+console.log(Number.isNaN(age));
+
+console.log(isNaN(name))
+console.log(isNaN(age))
+
+
+let arr =[1,2,3,4,5];
+let obj = {...arr}
+
+console.log(obj)
