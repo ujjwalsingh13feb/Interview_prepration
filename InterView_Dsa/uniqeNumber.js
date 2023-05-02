4c262c5e75c4cdd5021e01b4f721c06cf99828e9
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2023-05-02 14:16:04 +0530

    unique_Number_DSA

--- InterView_Dsa/uniqeNumber.js
+++ InterView_Dsa/uniqeNumber.js
@@ -3,19 +3,37 @@ let latters = ['a','b','a','a','d','b','c','b','d','e','f','g','g','h','f'];
 
 
 
-function uniqeNumber(arr){
-    let obj = {};
-    arr.forEach((element)=>{
-        if(!obj[element]){
-            obj[element] =1
-        }else{
-            obj[element]++
-        }
-    })
+// function uniqeNumber(arr){
+//     let obj = {};
+//     arr.forEach((element)=>{
+//         if(!obj[element]){
+//             obj[element] =1
+//         }else{
+//             obj[element]++
+//         }
+//     })
         
-     return obj
+//      return obj
+// }
+
+// let result =uniqeNumber(latters)
+
+// console.log(result)
+
+// second way to do the same question
+
+function unNumber(arr){
+  let obj = {};
+for(let i=0;i<arr.length;i++){
+    if(!obj[arr[i]]){
+        obj[arr[i]]=1;
+    }else{
+        obj[arr[i]]++
+    }
+}
+return obj
 }
 
-let result =uniqeNumber(latters)
+let resu = unNumber(latters);
 
-console.log(result)
+console.log(resu)
